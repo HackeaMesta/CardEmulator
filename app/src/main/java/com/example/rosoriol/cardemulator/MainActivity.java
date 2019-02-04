a8c0d379ae7e899b7b5c7790d36f3ffb983c4e1c
Ricardo <ricardo.osorio@previta.mx>
2019-02-04 15:43:01 -0600

    Hello World

---
+++ app/src/main/java/com/example/rosoriol/cardemulator/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.rosoriol.cardemulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText account = (EditText) findViewById(R.id.card_account_field);
+        account.setText(AccountStorage.GetAccount(this));
+        account.addTextChangedListener(new AccountUpdater());
+    }
+
+    private class AccountUpdater implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // Not implemented.
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // Not implemented.
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String account = s.toString();
+            AccountStorage.SetAccount(getApplicationContext(), account);
+        }
+    }
+
+}
